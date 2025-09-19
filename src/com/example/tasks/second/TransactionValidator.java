9539e342f41ff93699f16ccc76964cf7a20e8cfd
bru1f0rc3 <wint1841@inbox.ru>
2025-09-19 20:32:58 +0300

    ready

--- src/com/example/tasks/second/TransactionValidator.java
+++ src/com/example/tasks/second/TransactionValidator.java
@@ -5,13 +5,6 @@ public class TransactionValidator {
     public static final Integer MAX_AMOUNT = 5000;
 
     public static boolean isValidAmount(Double amount){
-        if (amount > 1 && amount < 5000) {
-            System.out.println("Минимальная сумма перевода: " + MIN_AMOUNT + " р. Попробуйте ещё раз!");
-            return true;
-        }
-        else{
-            System.out.println("Максимальная сумма перевода: " + MAX_AMOUNT + " р. Попробуйте ещё раз!");
-            return false;
-        }
+        return amount > MIN_AMOUNT && amount < MAX_AMOUNT;
     }
 }
